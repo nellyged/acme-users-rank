a73327eaaae967bbb9659f1a8f5f8b12155af8be
nelsonjlgedeon <nelson.gedeon@gmail.com>
2019-03-29 11:47:36 -0400

    Webpack setup

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express();
 const path = require('path');
 const { User } = require('./db');
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
@@ -13,6 +16,34 @@ app.get('/api/users', (req, res, next) => {
   User.findAll()
     .then(users => {
       res.send(users);
+    })
+    .catch(next);
+});
+
+app.put('/api/users/:id', (req, res, next) => {
+  User.findByPk(req.params.id)
+    .then(user => user.update(req.body))
+    .then(user => res.send(user))
+    .catch(next);
+});
+
+app.post('/api/users', (req, res, next) => {
+  console.log(req.body);
+  User.create(req.body)
+    .then(user => {
+      res.send(user);
+    })
+    .catch(next);
+});
+
+app.delete('/api/users/:id', (req, res, next) => {
+  User.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then(() => {
+      res.sendStatus(204);
     })
     .catch(next);
 });
--- db.js
+++ db.js
@@ -4,12 +4,18 @@ const conn = new Sequelize(process.env.DATABASE_URL, {
 });
 
 const User = conn.define('user', {
-  name: Sequelize.STRING,
+  name: { type: Sequelize.STRING, allowNull: false },
+  bio: Sequelize.TEXT,
+  rank: Sequelize.INTEGER,
 });
 
 const syncAndSeed = () => {
   return conn.sync({ force: true }).then(() => {
-    User.create({ name: 'Test' });
+    Promise.all([
+      User.create({ name: 'Moe', bio: 'CEO of Amce', rank: 1 }),
+      User.create({ name: 'Larry', bio: 'CTO of Acme', rank: 2 }),
+      User.create({ name: 'Curly', bio: 'CFO of Acme', rank: 3 }),
+    ]);
   });
 };
 
